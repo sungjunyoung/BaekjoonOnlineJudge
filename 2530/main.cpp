add630f33d473a3b56514229319206d8c2374f08
sungjunyoung <wnsdud1861@gmail.com>
2016-06-30 14:11:26 +0900

    2530

--- 2530/main.cpp
+++ 2530/main.cpp
@@ -3,6 +3,28 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    int h, m, s;
+    cin >> h >> m >> s;
+    int d;
+    cin >> d;
+
+    s = s + d;
+
+    while (s >= 60) {
+        s = s - 60;
+        m += 1;
+    }
+
+    while (m >= 60) {
+        m = m - 60;
+        h += 1;
+    }
+
+    while (h >= 24) {
+        h = h - 24;
+    }
+
+    cout << h << " " << m << " " << s;
     return 0;
 }
