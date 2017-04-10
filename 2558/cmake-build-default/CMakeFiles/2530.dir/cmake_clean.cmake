785c7cb0a1b32172799e102752c835cb171958cd
sungjunyoung <wnsdud1861@gmail.com>
2017-04-10 21:18:28 +0900

    2558

---
+++ 2558/cmake-build-default/CMakeFiles/2530.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2530.dir/main.cpp.o"
+  "2530.pdb"
+  "2530"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2530.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
