eed21a3d2fb91fc8298de5f732600f53d2b961c8
sungjunyoung <wnsdud1861@gmail.com>
2017-04-28 00:20:42 +0900

    2448 별찍기 드디어해결

--- 2448/main.cpp
+++ 2448/main.cpp
@@ -1,41 +1,52 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-char result_arr[7000][14000];
 
-int main() {
-    int N;
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < 2 * N - 1; j++)
-            result_arr[i][j] = ' ';
+void recursive(int n, char **a, int x, int y) {
+    if (n == 3) {
+        a[x][y + 2] = '*';
+        a[x + 1][y + 1] = '*';
+        a[x + 1][y + 3] = '*';
+        a[x + 2][y] = '*';
+        a[x + 2][y + 1] = '*';
+        a[x + 2][y + 2] = '*';
+        a[x + 2][y + 3] = '*';
+        a[x + 2][y + 4] = '*';
+        return;
     }
 
-    result_arr[0][N - 1] = '*';
-    result_arr[1][N - 2] = '*';
-    result_arr[1][N] = '*';
-    for (int i = 0; i < 5; i++)
-        result_arr[2][N - 3 + i] = '*';
-
-    if (N >= 6) {
-        int cnt = 1;
-        for (int i = 6; i <= N; i *= 2) {
-            for (int j = 0; j < i / 2; j++) {
-                for (int k = N - 1 - i / 2; k < N + i / 2; k++) {
-                    result_arr[i / 2 + j][k - i / 2] = result_arr[j][k];
-                    result_arr[i / 2 + j][k + i / 2] = result_arr[j][k];
-                }
-            }
-        }
+    else {
+        recursive(n/2, a, x, y+(2*n)/4);
+        recursive(n/2, a, x+n/2, y);
+        recursive(n/2, a, x+n/2, y+(2*n)/2);
+        return;
     }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < 2 * N - 1; j++)
-            cout << result_arr[i][j];
-        if (i < N - 1)
-            cout << " ";
-        cout << endl;
-    }
-    return 0;
 }
 
-// TODO 한번더보기
+int main() {
+    int n;
+    int i, j;
+    cin >> n;
+
+    char **a;
+    a = new char *[n];
+    for (i = 0; i < n; i++) {
+        a[i] = new char[2 * n - 1];
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < 2 * n - 1; j++) {
+            a[i][j] = ' ';
+        }
+    }
+
+    recursive(n, a, 0, 0);
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < 2 * n - 1; j++) {
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
+}
